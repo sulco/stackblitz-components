66a118facdb3d385426ff82f34f19541b43e9dbc
Tomek Sułkowski <tomek.sulkowski@gmail.com>
2024-07-19 15:04:47 +0200

    use css grid

--- src/components/SimpleEditor.tsx
+++ src/components/SimpleEditor.tsx
@@ -3,50 +3,58 @@ import CodeMirrorEditor from '@tutorialkit/components-react/core/CodeMirrorEdito
 import FileTree from '@tutorialkit/components-react/core/FileTree';
 import { lazy, Suspense, useEffect, useState } from 'react';
 
-const Terminal = lazy(() => import('@tutorialkit/components-react/core/Terminal'));
+const Terminal = lazy(
+  () => import('@tutorialkit/components-react/core/Terminal'),
+);
 
 export function SimpleEditor() {
   const [domLoaded, setDomLoaded] = useState(false);
 
-  const { setTerminal, previewSrc, document, files, onChange, onScroll, selectedFile, setSelectedFile } =
-    useSimpleEditor();
+  const {
+    setTerminal,
+    previewSrc,
+    document,
+    files,
+    onChange,
+    onScroll,
+    selectedFile,
+    setSelectedFile,
+  } = useSimpleEditor();
 
   useEffect(() => {
     setDomLoaded(true);
   }, []);
 
   return (
-    <div className="max-w-screen-lg mx-auto">
-      <div className="mx-4 my-4 h-[calc(100vh-2rem)] flex flex-col border border-gray-200 border-solid rounded-xl overflow-hidden">
-        <div className="flex h-1/2">
-          <FileTree
-            className="w-1/4 flex-shrink-0 text-sm mt-2"
-            files={files}
-            hideRoot
-            selectedFile={selectedFile}
-            onFileSelect={setSelectedFile}
-          />
-          <CodeMirrorEditor
+    <div className="w-full h-full overflow-hidden grid grid-rows-2 grid-cols-10 border-1 border-gray-200 rounded-xl">
+      <FileTree
+        className="grid-col-span-2 text-sm mt-2"
+        files={files}
+        hideRoot
+        selectedFile={selectedFile}
+        onFileSelect={setSelectedFile}
+      />
+      <CodeMirrorEditor
+        className="grid-col-span-8 w-full text-[13px] border-l-1 border-l-gray-200"
+        theme="light"
+        doc={document}
+        onChange={onChange}
+        onScroll={onScroll}
+      />
+      {domLoaded && (
+        <Suspense>
+          <Terminal
+            className="grid-col-span-3 border-t-1 border-t-gray-200"
+            readonly={false}
             theme="light"
-            doc={document}
-            onChange={onChange}
-            onScroll={onScroll}
-            className="h-full flex-grow max-w-[calc(75%-1px)] text-[13px] border-l-1 border-l-gray-200"
+            onTerminalReady={setTerminal}
           />
-        </div>
-        <div className="flex p-0 m-0 h-1/2 border-t-1 border-t-gray-200">
-          <div className="w-1/2 h-full">
-            {domLoaded && (
-              <Suspense>
-                <Terminal className="h-full" readonly={false} theme="light" onTerminalReady={setTerminal} />
-              </Suspense>
-            )}
-          </div>
-          <iframe className="w-1/2 h-full border-l-1 border-l-gray-200" src={previewSrc} />
-        </div>
-      </div>
+        </Suspense>
+      )}
+      <iframe
+        className="grid-col-span-7 border-l-1 w-full h-full border-l-gray-200 border-t-1 border-t-gray-200"
+        src={previewSrc}
+      />
     </div>
   );
 }
-
-
